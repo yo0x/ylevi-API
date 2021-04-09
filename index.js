e6c9bf54a8f676d86c0f0da44740ebe6d1b7c1c9
Yo0x <6yonor@gmail.com>
2021-04-09 15:27:10 +0000

    cors()

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const app = express();
 require("dotenv/config");
 //Middlewares
-// app.use(cors());
+var whitelist = ['http://ylevigroup.com','https://ylevigroup.com','https://scrm.ylevigroup.com', 'http://scrm.ylevigroup.com', 'https://localhost', 'http://localhost','http://ylevigroup.com/dbb','https://ylevigroup.com/dbb']
+app.use(cors(whitelist));
 app.use(function(req,res,next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -15,12 +16,12 @@ app.use(function(req,res,next){
 //import routes
 const projRoute = require('./routes/projectsr');
 app.use(bodyParser.json());
-app.use('/projects', cors(), projRoute);
+app.use('/projects', projRoute);
 //Routes
-app.get("/yonor",cors(), (req, res) => {
+app.get("/yonor", (req, res) => {
   res.send("Sir yonor top dev");
 });
-app.get("/posts",cors(), (req, res) => {
+app.get("/posts", (req, res) => {
   res.send("We are on home");
 });
 //Connect to DB
